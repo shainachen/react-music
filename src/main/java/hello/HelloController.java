9b2f64e2682001b34ea28b7fd0a85991cd00dc07
pivotal <pivotal@pivotals-iMac-4.local>
2017-06-27 11:03:09 -0700

    /albums endpoint linked to a datasource [#147818607]

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,7 +1,14 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 @RestController
 
@@ -13,7 +20,47 @@ public class HelloController {
     }
 
     @RequestMapping("/albums")
-    public String index1() {
-        return "woohoo";
+    public String index1() throws JSONException {
+        String filename = "lib/AlbumList.csv";
+        String line = "";
+        String cvsSplitBy = ",";
+        JSONObject finalAlbums = new JSONObject();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+
+            JSONArray albumList = new JSONArray();
+
+            int counter = 0;
+
+            while ((line = br.readLine()) != null) {
+
+                if (counter != 0) {
+
+                    // use comma as separator
+                    String[] albums = line.split(cvsSplitBy);
+
+                    JSONObject myAlbum = new JSONObject();
+                    myAlbum.put("genre", albums[3]);
+                    myAlbum.put("year", albums[2]);
+                    myAlbum.put("artist", albums[1]);
+                    myAlbum.put("name", albums[0]);
+
+
+                    albumList.put(myAlbum);
+                }
+                counter++;
+
+            }
+
+            finalAlbums.put("albums", albumList);
+
+
+            //System.out.print(finalAlbums);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return finalAlbums.toString();
+
     }
 }
