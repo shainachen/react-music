730070b0da09f972c58f020c35230bde8d57a49d
Shaina Chen <shaina.sierra@gmail.com>
2017-07-31 12:26:18 -0700

    Delete unused code
    Signed-off-by: Sandra Nissim <sandra.nissim@gmail.com>

--- src/main/java/App/configuration/InMemoryConfig.java
+++ src/main/java/App/configuration/InMemoryConfig.java
@@ -2,28 +2,20 @@ package App.configuration;
 
 import App.controllers.AlbumController;
 import App.domain.Albums;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.FileCopyUtils;
-
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @Component
-//@Profile("in-memory")
 @Profile("!mysqlcloud")
 public class InMemoryConfig implements DatasourceConfig {
 
-    //@Bean
     @Override
     public DataSource dataSource() {
         ClassPathResource filename = new ClassPathResource("AlbumList.csv");
@@ -46,11 +38,12 @@ public class InMemoryConfig implements DatasourceConfig {
         catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("in-mem setting up");
+
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        System.out.println("in-mem setting up");
+
         return builder.setType(EmbeddedDatabaseType.H2).build();
 
-        //return "Setting up in-memory datasource...";
     }
 
 }
